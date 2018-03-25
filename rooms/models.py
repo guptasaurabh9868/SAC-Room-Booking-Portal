0eba36be329649ab4109e931f71f60bc6803efc3
Rohit Prasad <rohitrp3@gmail.com>
2018-03-25 19:39:29 +0530

    Make number primary key

--- rooms/models.py
+++ rooms/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Room(models.Model):
-    number = models.IntegerField()
+    number = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100, blank=False)
 
     class Meta:
         ordering = ('number',)
+
+    def __unicode__(self):
+        return ' '.join([self.number, self.name])

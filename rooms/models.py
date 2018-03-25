f3b19e3dd7c7c05136a954ec02424afd2b561414
Rohit Prasad <rohitrp3@gmail.com>
2018-03-25 16:00:04 +0530

    Define room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        ordering = ('number',)

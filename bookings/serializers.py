ba5fb9670aa75df717f562a926bf99fb5bd16b33
Rohit Prasad <rohitrp3@gmail.com>
2018-03-26 15:07:08 +0530

    Implement serializer for user model

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from bookings.models import Booking
 from rooms.models import Room
+from django.contrib.auth.models import User
 
 class BookingSerilizer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Booking
-        fields = ('id', 'booking_from', 'booking_to', 'room_id')
+        fields = ('id', 'booking_from', 'booking_to', 'room_id', 'user')
+
+class UserSerializer(serializers.ModelSerializer):
+    bookings = serializers.PrimaryKeyRelatedField(many=True, queryset=Booking.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'bookings')

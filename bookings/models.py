e8ea8d4ee1c2d95ecbd35ee75d94a934e8a6a7cf
Rohit Prasad <rohitrp3@gmail.com>
2018-03-26 15:06:26 +0530

    Add user model

--- bookings/models.py
+++ bookings/models.py
@@ -5,7 +5,8 @@ class Booking(models.Model):
     booking_from = models.DateTimeField()
     booking_to = models.DateTimeField()
     room_id = models.ForeignKey('rooms.Room', related_name='bookings', on_delete=models.CASCADE)
-
+    user = models.ForeignKey('auth.User', related_name='bookings', on_delete=models.CASCADE)
+    
     class Meta:
         ordering = ['booking_from']
 

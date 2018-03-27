42e25610629a99ea1445a469c0c0663c6a0edc16
Rohit Prasad <rohitrp3@gmail.com>
2018-03-27 15:49:47 +0530

    Implement account manager class

--- authentication/models.py
+++ authentication/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class Account(AbstractBaseUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=40, unique=True)
 
     name = models.CharField(max_length=40, blank=True)
     is_admin = models.BooleanField(default=False)
@@ -15,10 +14,39 @@ class Account(AbstractBaseUser):
     objects = AccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     def __unicode__(self):
         return self.email
 
     def get_name(self):
         return self.name
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+
+        account = self.model(
+            email=self.normalize_email(email)
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs):
+
+        account.is_admin = True
+        account.save()
+
+        return account
+
+    def create_gsec_user(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs):
+
+        account.is_gsec = True
+        account.save()
+
+        return account

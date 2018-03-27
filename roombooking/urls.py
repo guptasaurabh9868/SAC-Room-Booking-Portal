5e19951560f385bec73182550e865a11868872cb
Rohit Prasad <rohitrp3@gmail.com>
2018-03-28 04:52:34 +0530

    Add link to show bookings in navbar

--- roombooking/urls.py
+++ roombooking/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from . import views 
+
+app_name = "roombooking"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),

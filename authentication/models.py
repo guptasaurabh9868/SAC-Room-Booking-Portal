7f95cc61aae1a4d5c9a0aa01da8ce1deea13e1c1
Rohit Prasad <rohitrp3@gmail.com>
2018-03-27 23:46:31 +0530

    Add permission mixin to account model

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
 
 class AccountManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
@@ -12,7 +13,7 @@ class AccountManager(BaseUserManager):
         account = self.model(
             email=self.normalize_email(email),
             username=kwargs.get('username'),
-            name=kwargs.get('name')
+            name=kwargs.get('name', '')
         )
 
         account.set_password(password)
@@ -36,15 +37,16 @@ class AccountManager(BaseUserManager):
 
         return account
 
-class Account(AbstractBaseUser):
+class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=40, unique=True)
 
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, default="")
     is_admin = models.BooleanField(default=False)
     is_gsec = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
-
+    is_staff = models.BooleanField(default=False)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

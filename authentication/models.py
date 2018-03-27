5e5a61af5c7de7d87736abc81ccf657cc2b07078
Rohit Prasad <rohitrp3@gmail.com>
2018-03-27 15:55:59 +0530

    Bug fix

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+
+        account = self.model(
+            email=self.normalize_email(email)
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
+
+    def create_gsec_user(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_gsec = True
+        account.save()
+
+        return account
+        
 class Account(AbstractBaseUser):
     email = models.EmailField(unique=True)
 
@@ -20,33 +50,3 @@ class Account(AbstractBaseUser):
 
     def get_name(self):
         return self.name
-
-class AccountManager(BaseUserManager):
-    def create_user(self, email, password=None, **kwargs):
-        if not email:
-            raise ValueError('Users must have a valid email address.')
-
-        account = self.model(
-            email=self.normalize_email(email)
-        )
-
-        account.set_password(password)
-        account.save()
-
-        return account
-
-    def create_superuser(self, email, password, **kwargs):
-        account = self.create_user(email, password, **kwargs):
-
-        account.is_admin = True
-        account.save()
-
-        return account
-
-    def create_gsec_user(self, email, password, **kwargs):
-        account = self.create_user(email, password, **kwargs):
-
-        account.is_gsec = True
-        account.save()
-
-        return account
